2aa05442942b021ba258cc30db77dac5eb0948ce
Allegro <alex.makovka@gmail.com>
2023-05-24 11:33:43 +0300

    solution

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
+import _ from 'lodash';
+
+const makeDataArray = (content) => {
+  const strings = content.split('\n').slice(2);
+  const array = strings.map((string) =>
+    string
+      .split('|')
+      .filter((el) => el !== '')
+      .map((el) => el.trim()),
+  );
+  return _.sortBy(array.map(([herb, ...rest]) => [_.capitalize(herb), ...rest]));
+};
+
+const getDangerousPercents = (dataArray) => {
+  const herbsCount = dataArray.length;
+  const dangerous = dataArray.reduce((acc, herbString) => {
+    return herbString[herbString.length - 1] === 'Да' ? acc + 1 : acc;
+  }, 0);
+  const notDangerous = herbsCount - dangerous;
+  return [`${(dangerous / herbsCount) * 100}%`, `${(notDangerous / herbsCount) * 100}%`];
+};
+
+const getAverageLife = (dataArray) => {
+  const lifesValues = dataArray.map((herbString) => herbString[3].slice(0, -4).split('-'));
+  const lifesValuesFilled = lifesValues.map(([from, to]) => {
+    if (!to) return [from, from];
+    return [from, to];
+  });
+
+  const herbsCount = dataArray.length;
+  const fullLife = lifesValuesFilled.reduce(
+    (acc, [from, to]) => {
+      const [accFrom, accTo] = acc;
+      return [accFrom + Number(from), accTo + Number(to)];
+    },
+    [0, 0],
+  );
+  const [fullLifeFrom, fullLifeTo] = fullLife;
+  return `${fullLifeFrom / herbsCount}-${fullLifeTo / herbsCount}`;
+};
+
+const getDangerousArea = (dataArray) => {
+  const dangAreas = dataArray.filter((herbString) => herbString[herbString.length - 1] === 'Да').map((herbString) => herbString[1]);
+  const unicDangAreas = _.union(dangAreas);
+  const areasCount = unicDangAreas.reduce((acc, area) => {
+    const curAreaCount = dangAreas.reduce((curAcc, curArea) => {
+      return curArea === area ? curAcc + 1 : curAcc;
+    }, 0);
+    return [...acc, [area, curAreaCount]];
+  }, []);
+
+  const [dangArea] = _.sortBy(areasCount, 1)[areasCount.length - 1];
+  return dangArea;
+};
+
 export default function solution(content) {
   // BEGIN
-  console.log(content)
+  const dataArray = makeDataArray(content);
+
+  const herbsCount = dataArray.length;
+  console.log(herbsCount);
+
+  const [dangerousPercent, notDangerousPercent] = getDangerousPercents(dataArray);
+  console.log(dangerousPercent);
+  console.log(notDangerousPercent);
+
+  const averageLife = getAverageLife(dataArray);
+  console.log(averageLife);
+
+  const dangerousArea = getDangerousArea(dataArray);
+  console.log(dangerousArea);
+
   // END
 }
